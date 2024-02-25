2f4f8b9b8ae5b51bfa680e5d6624266d64e995e8
Hemaxi <merchanthemaxi@gmail.com>
2024-02-25 10:06:37 +0530

    print statement changed

--- band name generator.py
+++ band name generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band name generator!")
+print("Hello wassup")
 city=input("What city did you grow up in?\n")
 name_pet=input("What is the name of your pet?\n")
 print("Your band name could be",city + " " + name_pet)
